62ed49457313ab880cee6411978397b9e1b78c27
BLACK-ZERO <madman4205666@gmail.com>
2025-09-17 18:31:37 +0600

    Update MJOLNIR.py

--- MJOLNIR.py
+++ MJOLNIR.py
@@ -1,14 +1,13 @@
 import os
 import time
 def info():
-    print("\033[92m\033[1m [🔥] MJOLNIR DDoS STARTed")
-    time.sleep(1.5)
+    print("\n\n\033[92m\033[1m [🔥] MJOLNIR DDoS STARTed")
+    time.sleep(0.9)
 def start():
-    os.system("./MJOLNIR")
+    os.system("python3 MJOLNIR")
 def run():
     info()
     start()
 
 if __name__ == "__main__":
    run()
-    
